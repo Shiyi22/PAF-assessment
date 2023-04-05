336c2b96aef909b12256f724b54a93c7e1ec5cdd
Shiyi <yeoshiyi22@gmail.com>
2023-04-05 10:50:17 +0800

    task 5 done

--- database/schema.sql
+++ database/schema.sql
@@ -9,3 +9,14 @@ create table user (
     name varchar(15),
 	constraint user_pk primary key(user_id) 
 ); 
+
+-- Task 5 
+create table task (
+	task_id int auto_increment, 
+    user_id varchar(8) not null, 
+    description varchar(255), 
+    priority int, 
+    due_date Date,
+    constraint task_pk primary key(task_id),
+    constraint task_user_fk foreign key(user_id) references user(user_id)
+); 
