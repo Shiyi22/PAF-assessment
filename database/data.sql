a30c1a560d21a22c1011023ffc8431db1de987c2
Shiyi <yeoshiyi22@gmail.com>
2023-04-05 09:36:44 +0800

    task 3 done

--- database/data.sql
+++ database/data.sql
@@ -2,3 +2,7 @@ insert into user values ('1b80114c', 'fred', 'Fred');
 insert into user values ('cf66dae3', 'wilma', 'Wilma'); 
 insert into user values ('a8b9800d', 'barney', 'Barney'); 
 insert into user values ('66223e28', 'betty', 'Betty'); 
+
+-- SQL queries 
+-- select * from user where username = 'fred'; 
+-- insert into user values ('', '', ''); 
--- src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
+++ src/main/java/ibf2022/paf/assessment/server/repositories/UserRepository.java
@@ -1,6 +1,42 @@
 package ibf2022.paf.assessment.server.repositories;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import ibf2022.paf.assessment.server.models.User;
+
 // TODO: Task 3
 
 public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate; 
+
+    private static final String FIND_USER_BY_USERNAME_SQL = "select * from user where username = ?"; 
+    private static final String INSERT_USER_SQL = "insert into user values (?, ?, ?)"; 
+
+    // method 1 
+    public Optional<User> findUserByUsername(String username) {
+        
+        User user = jdbcTemplate.queryForObject(FIND_USER_BY_USERNAME_SQL, BeanPropertyRowMapper.newInstance(User.class), username); 
+        if (null == user)
+            return Optional.empty(); 
+        System.out.printf(">>> user: %s\n", user.toString()); 
+        return Optional.of(user); 
+    }
+
+    // method 2 
+    public String insertUser(User user) {
+
+        // generate random user_id for new user 
+        String userId = UUID.randomUUID().toString().substring(0, 8); 
+        
+        // insert into database 
+        jdbcTemplate.update(INSERT_USER_SQL, userId, user.getUsername(), user.getName()); 
+        return userId; 
+    }
 }
